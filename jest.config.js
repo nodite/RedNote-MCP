5e8e206152f6b2df7ff1b4e17ed8dcd7332f4b86
Oscaner Miao <oscaner1997@gmail.com>
2026-03-31 16:10:31 +0800

    test: add tsconfig.test.json and configure ts-jest to use it

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   transform: {
-    '^.+\\.tsx?$': 'ts-jest'
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.test.json' }]
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
 };
